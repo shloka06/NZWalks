0102b654bf25161b392bd4f6b5b13db4ae7b8b2f
Shloka Jaywant <shlokajaywant@gmail.com>
2024-07-17 11:06:53 +0530

    Added POST method for Region.

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -17,8 +17,6 @@ namespace NZWalks.API.Controllers
             this.dbContext = dbContext;
         }
 
-        
-
         // GET ALL REGIONS
         // GET: https://localhost:1234/api/regions
         [HttpGet]
@@ -70,6 +68,35 @@ namespace NZWalks.API.Controllers
             }
 
             return NotFound();
+        }
+
+        // POST REGION
+        // POST: https://localhost:1234/api/regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map/Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // Use Domain Model to Create 
+            dbContext.Region.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            // Map Domain Model back to DTO
+            var regionDto = new RegionDto 
+            { 
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDto);
         }
     }
 }
